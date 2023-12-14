c24ba70df1cfb37970ec4f72bde1446dc7e4f2cd
mrkroms <spklgd@gmail.com>
2023-12-14 09:13:37 +0200

    creat tabs js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", () => {
+  const tabs = document.querySelectorAll(".tabheader__item");
+  const tabContainer = document.querySelector(".tabcontainer");
+  const tabContents = document.querySelectorAll(".tabcontainer");
+});
