8ff573d360845388f4738b2fca00650bbc2b8337
mrkroms <spklgd@gmail.com>
2024-01-04 20:07:40 +0200

    add slider v1.0

--- js/script.js
+++ js/script.js
@@ -400,4 +400,50 @@ window.addEventListener("DOMContentLoaded", () => {
   fetch("http://localhost:3000/menu")
     .then((data) => data.json())
     .then((res) => console.log(res));
+
+  // slider
+  const slides = document.querySelectorAll(".offer__slide");
+  const prev = document.querySelector(".offer__slider-prev");
+  const next = document.querySelector(".offer__slider-next");
+  let slideIndex = 1;
+
+  const total = document.querySelector("#total");
+  const current = document.querySelector("#current");
+
+  showSlide(slideIndex);
+
+  if (slides.length < 10) {
+    total.textContent = `0${slides.length}`;
+  } else {
+    total.textContent = slides.length;
+  }
+
+  function showSlide(n) {
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+    slides.forEach((item) => (item.style.display = "none"));
+    slides[slideIndex - 1].style.display = "block";
+
+    if (slides.length < 10) {
+      current.textContent = `0${slideIndex}`;
+    } else {
+      current.textContent = slideIndex;
+    }
+  }
+
+  function plusSlides(n) {
+    showSlide((slideIndex += n));
+  }
+
+  prev.addEventListener("click", () => {
+    plusSlides(-1);
+  });
+
+  next.addEventListener("click", () => {
+    plusSlides(1);
+  });
 });
